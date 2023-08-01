90a7959af905ce35b44386c7ae2672bcd9a7a42c
Rahmat <slow9ie@gmail.com>
2023-08-01 22:24:34 +0700

    debug: create event

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -179,6 +179,8 @@ class UserController extends Controller
             ? config('constants.event_registrant_role.individual')
             : config('constants.event_registrant_role.leader');
 
+        Log::channel('api')->info($role);
+
         // Attach current authenticated user to the registration with determined role
         $eventRegistration->users()->attach($request->user()->id, ['role' => $role]);
 
